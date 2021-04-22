f21fabb6a06be47fc0df3821886f0d1355517cff
yunjedi <kingofdragonjacklong2009@gmail.com>
2021-04-22 20:20:30 +0700

    RecyclerView for restaurant

---
+++ app/src/main/java/com/example/baitaptuan1/restaurant/RestaurantAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.baitaptuan1.restaurant
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
+import com.example.baitaptuan1.R
+
+class RestaurantAdapter : ListAdapter<Restaurant,RestaurantAdapter.ViewHolder>(RestaurantDiffUtilCallback()) {
+
+    interface RestaurantAdapterListener{
+        fun onClickItem(restaurant: Restaurant)
+    }
+    var listener : RestaurantAdapterListener? = null
+
+    class RestaurantDiffUtilCallback : DiffUtil.ItemCallback<Restaurant>(){
+        override fun areItemsTheSame(oldItem: Restaurant, newItem: Restaurant): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Restaurant, newItem: Restaurant): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, listener)
+    }
+
+    class ViewHolder private constructor(val itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tvRestaurantName = itemView.findViewById<TextView>(R.id.itemTitle)
+        val tvRestaurantAddress = itemView.findViewById<TextView>(R.id.itemDetail)
+        val imgRestaurant = itemView.findViewById<ImageView>(R.id.itemImage)
+
+        companion object {
+            fun from(parent: ViewGroup) : ViewHolder {
+                var layoutInflater = LayoutInflater.from(parent.context)
+                var view = layoutInflater.inflate(R.layout.itemrestaurants, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
+        fun bind(item: Restaurant, listener: RestaurantAdapterListener?) {
+            tvRestaurantName.text = item.name
+            tvRestaurantAddress.text = item.address
+            Glide.with(itemView.context)
+                .load(item.picturePath)
+                .into(imgRestaurant)
+
+            itemView.setOnClickListener {
+                listener?.onClickItem(item)
+            }
+        }
+    }
+
+}
+
+
